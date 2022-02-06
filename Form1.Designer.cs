95a96e3cbef1a218672c960ca5a27c2c1ae70dea
ashiya-viswanath <46847130+ashiya-viswanath@users.noreply.github.com>
2022-02-06 14:41:22 +0100

    flappy bird

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace birdgame
+{
+    partial class birdGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(birdGame));
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeDown = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipeUp = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeUp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-12, 489);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(815, 50);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // pipeDown
+            // 
+            this.pipeDown.Image = ((System.Drawing.Image)(resources.GetObject("pipeDown.Image")));
+            this.pipeDown.Location = new System.Drawing.Point(521, 363);
+            this.pipeDown.Name = "pipeDown";
+            this.pipeDown.Size = new System.Drawing.Size(100, 130);
+            this.pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeDown.TabIndex = 1;
+            this.pipeDown.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(33, 212);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(88, 71);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 2;
+            this.bird.TabStop = false;
+            // 
+            // pipeUp
+            // 
+            this.pipeUp.Image = ((System.Drawing.Image)(resources.GetObject("pipeUp.Image")));
+            this.pipeUp.Location = new System.Drawing.Point(521, -1);
+            this.pipeUp.Name = "pipeUp";
+            this.pipeUp.Size = new System.Drawing.Size(100, 196);
+            this.pipeUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeUp.TabIndex = 3;
+            this.pipeUp.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scoreText.Location = new System.Drawing.Point(73, 64);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(83, 25);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score : 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.startGame);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 375);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.AcceptsTabChanged += new System.EventHandler(this.textBox1_AcceptsTabChanged);
+            // 
+            // birdGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 539);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.pipeUp);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.pipeDown);
+            this.Controls.Add(this.ground);
+            this.Name = "birdGame";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.birdGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.birdGame_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeUp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipeDown;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox pipeUp;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace birdgame
+{
+    public partial class birdGame : Form
+    {
+        int score = 0;
+        int gravity = 10;
+        int pipeSpeed = 4;
+        public birdGame()
+        {
+            InitializeComponent();
+        }
+
+        private void startGame(object sender, EventArgs e)
+         {
+            //scoreText.Text = bird.Top.ToString();
+            bird.Top += gravity;
+           
+             pipeDown.Left -= pipeSpeed; // link the bottom pipes left position to the pipe speed integer, it will reduce the pipe speed value from the left position of the pipe picture box so it will move left with each tick
+            pipeUp.Left -= pipeSpeed; // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
+           
+            scoreText.Text = "Score: " + score; // sh
+         
+            if (pipeDown.Left < -150)
+            {
+                // if the bottom pipes location is -150 then we will reset it back to 800 and add 1 to the score
+                pipeDown.Left = 800;
+                score++;
+            }
+             if (pipeUp.Left < -180)
+            {
+                // if the top pipe location is -180 then we will reset the pipe back to the 950 and add 1 to the score
+                pipeUp.Left = 950;
+                score++;
+            }
+           
+             if (bird.Bounds.IntersectsWith(pipeDown.Bounds) ||
+               bird.Bounds.IntersectsWith(pipeUp.Bounds) ||
+               bird.Bounds.IntersectsWith(ground.Bounds) || bird.Top < -25
+               )
+            {
+                // if any of the conditions are met from above then we will run the end game function
+                endGame();
+            }
+            void endGame()
+            {
+                timer1.Stop(); // stop the main timer
+                scoreText.Text += " Game over!!!";
+
+            }
+        }
+
+        private void birdGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+
+        }
+
+        private void birdGame_KeyDown(object sender, KeyEventArgs e)
+          {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+            }
+
+        }
+
+        private void textBox1_AcceptsTabChanged(object sender, EventArgs e)
+        {
+
+        }
+    }    
+}
